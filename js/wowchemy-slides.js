70274b2f49e7eba745ceda8dd0a55761c20f70df
Soteris Demetriou <soterisdemetriou@Soteriss-MacBook-Pro-2.local>
2026-03-26 11:14:39 +0000

    Update security config and publication content

--- js/wowchemy-slides.js
+++ js/wowchemy-slides.js
@@ -1,8 +1,8 @@
 (() => {
   // ns-params:@params
-  var slides = {highlight_style: "dracula", theme: "black"};
+  var slides = { highlight_style: "dracula", theme: "black" };
 
-  // ns-hugo:/var/folders/z4/xptxxt090td9gf0l7pfg0p580000gn/T/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy@v0.0.0-20210303111450-5529404d1942/assets/js/wowchemy-utils.js
+  // ns-hugo:/var/folders/qj/mvyp6nws2fb5th2rr_b83_th0000gn/T/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy@v0.0.0-20210303111450-5529404d1942/assets/js/wowchemy-utils.js
   function fixMermaid() {
     let mermaids = [];
     [].push.apply(mermaids, document.getElementsByClassName("language-mermaid"));
@@ -61,7 +61,16 @@
       var background = mslide.slideBackgroundElement;
       var currentHorizontalIndex = Reveal.getState()["indexh"];
       var diagramSlideIndex = Reveal.getIndices(mslide)["h"];
-      if (!diag.hasAttribute("data-processed") && background.hasAttribute("data-loaded") && background.style.display === "block" && diagramSlideIndex - currentHorizontalIndex <= 1)
+      if (
+        // find slides with non-rendered mermaid tags
+        // these will not have the attribute data-processed
+        !diag.hasAttribute("data-processed") && // check also that reveal slide is already loaded
+        // reveal slides seem to be lazily loaded
+        // things could be easier if reveal had a slide-loaded event
+        background.hasAttribute("data-loaded") && // loaded slides must also have the display attribute set to block
+        background.style.display === "block" && // render diagrams that are 1 slide away
+        diagramSlideIndex - currentHorizontalIndex <= 1
+      )
         return mslide;
     }
     return null;
